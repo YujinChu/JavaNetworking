839c5e6959699af71dac8d19836df1f9a2bab3d2
mirim <mirim@e-mirim.hs.kr>
2015-09-10 17:24:21 +0900

    add UDP_Java Networking_0910

---
+++ UDP/src/UDPClientMain.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by LG on 2015-09-10.
+ */
+public class UDPClientMain {
+    public static void main(String[] args) {
+        String tmpS = "";
+        //input message
+        System.out.print("보낼 메시지 > ");
+        Scanner sc = new Scanner(System.in);
+        tmpS = sc.nextLine();
+        
+        //make packet
+        //make socket
+        //send
+    }
+}
---
+++ UDP/src/UDPServerMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by LG on 2015-09-10.
+ */
+public class UDPServerMain {
+    //make socket
+    //make packet
+    //receive
+    //print message
+}
