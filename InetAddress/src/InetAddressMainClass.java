2daf39c2e4750dd5dd2bac155c6a8f9f553646d2
mirim <mirim@e-mirim.hs.kr>
2015-09-10 16:56:31 +0900

    InetAddress_Java Networking_0910

--- InetAddress/src/InetAddressMainClass.java
+++ InetAddress/src/InetAddressMainClass.java
@@ -6,9 +6,8 @@ import java.net.UnknownHostException;
  */
 public class InetAddressMainClass {
     public static void main(String[] args) throws UnknownHostException{
-        //내 컴퓨터
+        //내 컴퓨터    get InetAddress from 127.0.0.1
         System.out.println("내 컴퓨터");
-        //get InetAddress from 127.0.0.1
         InetAddress myip = InetAddress.getLocalHost();
         //IP 주소
         System.out.println("IP 주소 : " + myip.getHostAddress());
